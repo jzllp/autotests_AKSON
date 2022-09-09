9d3d111c90cc5a91b8a05cfbbd94f6e5b6150511
Jony <99526215+jzllp@users.noreply.github.com>
2022-09-09 11:10:08 +0300

    added update

--- tests/03_test_Search_menu.py
+++ tests/03_test_Search_menu.py
@@ -6,7 +6,7 @@
 #  3) Run in terminal with line:
 #     python -m pytest -v --driver Chrome --driver-path <~>/chromedriver.exe tests/03_test_Search_menu.py
 
-from pages.akson_locators import Header_sticky, Header_top
+from pages.akson_locators import Header_sticky, Header_top, Card_product
 
 
 def test_StartPage_search_menu_01(web_browser):
@@ -166,6 +166,20 @@ def test_StartPage_search_menu_12(web_browser):
 
 
 def test_StartPage_search_menu_13(web_browser):
+    """ Поиск товара по коду. """
+
+    page = Header_sticky(web_browser)
+    page_city = Header_top(web_browser)
+    page_gard = Card_product(web_browser)
+
+    page_city.choice_location.click()
+    page.search_menu = "130980"
+    page.search_button.click()
+
+    assert page_gard.card_code.get_text().find('130980') != -1, 'Неверный товар в результате поиска'
+
+
+def test_StartPage_search_menu_14(web_browser):
     """ Проверка меню поиска значением из цифр. """
 
     page = Header_sticky(web_browser)
@@ -179,7 +193,7 @@ def test_StartPage_search_menu_13(web_browser):
     assert page.result_all_goods.is_presented() is False, 'Поиск нашел неверное значение'
 
 
-def test_StartPage_search_menu_14(web_browser):
+def test_StartPage_search_menu_15(web_browser):
     """ Проверка меню поиска значением с пробелами. """
 
     page = Header_sticky(web_browser)
@@ -192,7 +206,7 @@ def test_StartPage_search_menu_14(web_browser):
     assert page.result_product.get_text().find('Люстра') != -1, 'Неверный товар в результате поиска'
 
 
-def test_StartPage_search_menu_15(web_browser):
+def test_StartPage_search_menu_16(web_browser):
     """ Проверка меню поиска пустым значением. """
 
     page = Header_sticky(web_browser)
@@ -205,7 +219,7 @@ def test_StartPage_search_menu_15(web_browser):
     assert page.result_all_goods.is_presented() is False, 'Поиск нашел неверное значение'
 
 
-def test_StartPage_search_menu_16(web_browser):
+def test_StartPage_search_menu_17(web_browser):
     """ Проверка активации меню поиска без значения. """
 
     page = Header_sticky(web_browser)
@@ -216,7 +230,7 @@ def test_StartPage_search_menu_16(web_browser):
     assert page.search_button.is_presented() is False, 'Поиск активен'
 
 
-def test_StartPage_search_menu_17(web_browser):
+def test_StartPage_search_menu_18(web_browser):
     """ Проверка меню поиска, ввод значения в 49 символов. """
 
     page = Header_sticky(web_browser)
@@ -230,7 +244,7 @@ def test_StartPage_search_menu_17(web_browser):
         'Количество символов в воде и в результате поиска не совпадают'
 
 
-def test_StartPage_search_menu_18(web_browser):
+def test_StartPage_search_menu_19(web_browser):
     """ Проверка меню поиска, ввод значения в 50 символов. """
 
     page = Header_sticky(web_browser)
@@ -244,7 +258,7 @@ def test_StartPage_search_menu_18(web_browser):
         'Количество символов в воде и в результате поиска не совпадают'
 
 
-def test_StartPage_search_menu_19(web_browser):
+def test_StartPage_search_menu_20(web_browser):
     """ Проверка меню поиска, ввод значения в 99 символов. """
 
     page = Header_sticky(web_browser)
